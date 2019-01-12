53e1297ae8675de5d311b4ddaa53c9c52a22d14b
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-12 14:16:27 -0800

    added exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,14 @@
+var exec = require("child_process").exec;
+
 function start() {
     console.log("Request handler 'start' was called");
-    function sleep(miliSeconds) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + miliSeconds);
-    }
+    var content = "empty";
+    
+    exec("ls -lah", function (error, stdout, stderr) {
+        content = stdout;
+    });
 
-    sleep(10000);
-    return "Hello Start"
+    return content;
 }
 
 function upload() {
